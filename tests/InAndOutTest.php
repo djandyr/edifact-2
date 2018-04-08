7bc9b3f1b2ebf1173d12c8661f87a70a9281c2a2
Craig Duncan <craig@metroplexsystems.co.uk>
2018-04-08 15:37:18 +0100

    Create a class to handle tradacoms control characters

--- tests/InAndOutTest.php
+++ tests/InAndOutTest.php
@@ -2,7 +2,10 @@
 
 namespace Metroplex\EdifactTests;
 
+use Metroplex\Edifact\Control\Tradacoms;
 use Metroplex\Edifact\Message;
+use Metroplex\Edifact\Parser;
+use Metroplex\Edifact\Serializer;
 
 class CompleteTest extends \PHPUnit_Framework_TestCase
 {
@@ -21,6 +24,21 @@ class CompleteTest extends \PHPUnit_Framework_TestCase
         $output = (string) Message::fromFile($file);
 
         $message = file_get_contents($file);
+        $expected = str_replace("\n", "", $message);
+
+        $this->assertSame($expected, $output);
+    }
+
+
+    public function testTradacoms()
+    {
+        $message = file_get_contents(__DIR__ . "/data/tradacoms.edi");
+
+        $tradacoms = new Tradacoms;
+        $segments = (new Parser)->parse($message, $tradacoms);
+
+        $output = (new Serializer($tradacoms))->serialize(...$segments);
+
         $expected = str_replace("\n", "", $message);
 
         $this->assertSame($expected, $output);
